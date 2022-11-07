f4e0acd896cbdc0ae0be7acddd1fcda716e4489a
Kitson Kelly <me@kitsonkelly.com>
2022-11-08 08:00:56 +1100

    feat: harden build script

--- build.ts
+++ build.ts
@@ -13,65 +13,87 @@ interface DocCodeFixFrontMatter {
   title: string;
 }
 
-const docs = new Set<number>();
-const docFixes = new Map<number, string[]>();
-
-const CODE_RE = /^(\d{4})\.md$/;
-const FIX_RE = /^(\d{4})_fix_(\d{2})\.md$/;
-
-for await (const entry of Deno.readDir("./docs")) {
-  const codeMatch = CODE_RE.exec(entry.name);
-  const fixMatch = FIX_RE.exec(entry.name);
-  if (codeMatch) {
-    const [, codeStr] = codeMatch;
-    const code = parseInt(codeStr, 10);
-    docs.add(code);
-  } else if (fixMatch) {
-    const [, codeStr, fix] = fixMatch;
-    const code = parseInt(codeStr, 10);
-    if (!docFixes.has(code)) {
-      docFixes.set(code, []);
-    }
-    docFixes.get(code)!.push(fix);
-  } else {
-    $.logWarn(`Unsupported file located at: /docs/${entry.name}`);
-  }
+function stringify(value: unknown) {
+  return `${JSON.stringify(value, undefined, "  ")}\n`;
 }
 
-const all: DiagnosticData[] = [];
-const index: Record<number, string> = {};
+async function main() {
+  const docs = new Set<number>();
+  const docFixes = new Map<number, string[]>();
 
-for (const code of docs) {
-  const md = await Deno.readTextFile(`./docs/${code}.md`);
-  const {
-    body: documentation,
-    attrs: { title, tags, related },
-  } = extract<DocCodeFrontMatter>(md);
-  index[code] = title;
-  const fixIds = docFixes.get(code);
-  let fixes: DiagnosticFixData[] | undefined;
-  if (fixIds) {
-    fixes = [];
-    for (
-      const fixId of fixIds.sort((a, b) => parseInt(a, 10) - parseInt(b, 10))
-    ) {
-      const md = await Deno.readTextFile(`./docs/${code}_fix_${fixId}.md`);
-      const { body, attrs: { title } } = extract<DocCodeFixFrontMatter>(md);
-      fixes.push({ title, body });
+  const CODE_RE = /^(\d{4})\.md$/;
+  const FIX_RE = /^(\d{4})_fix_(\d{2})\.md$/;
+
+  $.logStep("Analyzing /docs...");
+  $.logGroup();
+
+  for await (const entry of Deno.readDir("./docs")) {
+    const codeMatch = CODE_RE.exec(entry.name);
+    const fixMatch = FIX_RE.exec(entry.name);
+    if (codeMatch) {
+      const [, codeStr] = codeMatch;
+      const code = parseInt(codeStr, 10);
+      docs.add(code);
+    } else if (fixMatch) {
+      const [, codeStr, fix] = fixMatch;
+      const code = parseInt(codeStr, 10);
+      if (!docFixes.has(code)) {
+        docFixes.set(code, []);
+      }
+      docFixes.get(code)!.push(fix);
+    } else {
+      $.logWarn(`Unsupported file located at: /docs/${entry.name}`);
     }
   }
-  all.push({ code, title, documentation, tags, related, fixes });
+
+  $.logLight(`located ${docs.size} diagnostic docs.`);
+  $.logGroupEnd();
+
+  const all: DiagnosticData[] = [];
+  const index: Record<number, string> = {};
+  const writePromises: Promise<void>[] = [];
+
+  $.logStep(`Parsing diagnostic documentation...`);
+
+  for (const code of docs) {
+    const md = await Deno.readTextFile(`./docs/${code}.md`);
+    const {
+      body: documentation,
+      attrs: { title, tags, related },
+    } = extract<DocCodeFrontMatter>(md);
+    index[code] = title;
+    const fixIds = docFixes.get(code);
+    let fixes: DiagnosticFixData[] | undefined;
+    if (fixIds) {
+      fixes = [];
+      for (
+        const fixId of fixIds.sort((a, b) => parseInt(a, 10) - parseInt(b, 10))
+      ) {
+        const md = await Deno.readTextFile(`./docs/${code}_fix_${fixId}.md`);
+        const { body, attrs: { title } } = extract<DocCodeFixFrontMatter>(md);
+        fixes.push({ title, body });
+      }
+    }
+    const diagnostic = { code, title, documentation, tags, related, fixes };
+    writePromises.push(
+      Deno.writeTextFile(`./db/${code}.json`, stringify(diagnostic)),
+    );
+    all.push(diagnostic);
+  }
+
+  all.sort(({ code: aCode }, { code: bCode }) => aCode - bCode);
+
+  $.logStep("Writing document db...");
+
+  await Promise.all([
+    ...writePromises,
+    Deno.writeTextFile("./db/_all.json", stringify(all)),
+    Deno.writeTextFile("./db/_index.json", stringify(index)),
+  ]);
 }
 
-all.sort(({ code: aCode }, { code: bCode }) => aCode - bCode);
+$.logStep("Done.");
 
-await Promise.all([
-  Deno.writeTextFile(
-    "./db/_all.json",
-    JSON.stringify(all, undefined, "  "),
-  ),
-  Deno.writeTextFile(
-    "./db/_index.json",
-    JSON.stringify(index, undefined, "  "),
-  ),
-]);
+if (import.meta.main) {
+  main();
+}
