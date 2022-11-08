53eb2309b50bc32674d065faf079f570d06821c6
Kitson Kelly <me@kitsonkelly.com>
2022-11-08 15:04:00 +1100

    feat: scaffold diagnostic page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,12 +4,14 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_app.tsx";
-import * as $1 from "./routes/index.tsx";
+import * as $1 from "./routes/code.tsx";
+import * as $2 from "./routes/index.tsx";
 
 const manifest = {
   routes: {
     "./routes/_app.tsx": $0,
-    "./routes/index.tsx": $1,
+    "./routes/code.tsx": $1,
+    "./routes/index.tsx": $2,
   },
   islands: {},
   baseUrl: import.meta.url,
---
+++ routes/code.tsx
@@ -0,0 +1,42 @@
+import { Head } from "$fresh/runtime.ts";
+import {
+  type Handlers,
+  type PageProps,
+  type RouteConfig,
+} from "$fresh/server.ts";
+import type { DiagnosticData } from "$types";
+import { Diagnostic } from "../components/Diagnostic.tsx";
+
+type Data = DiagnosticData;
+
+export default function DiagnosticPage({ data }: PageProps<Data>) {
+  return (
+    <>
+      <Head>
+        <title>tswhy?</title>
+      </Head>
+      <div class="p-4 mx-auto max-w-screen-lg">
+        <Diagnostic>{data}</Diagnostic>
+      </div>
+    </>
+  );
+}
+
+export const handler: Handlers<Data> = {
+  async GET(_req, { params: { code }, render }) {
+    try {
+      const res = await fetch(new URL(`../db/${code}.json`, import.meta.url));
+      if (res.status === 200) {
+        const data: DiagnosticData = await res.json();
+        return render(data);
+      }
+    } catch {
+      //
+    }
+    return new Response("not found", { status: 404, statusText: "Not found" });
+  },
+};
+
+export const config: RouteConfig = {
+  routeOverride: "/ts:code",
+};
